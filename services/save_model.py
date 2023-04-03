87e3e9422e27ab3ed1d5797c46c0848a7ce6b5cb
Taeef Najib <taeefnajib@gmail.com>
2023-04-03 07:33:56 +0000

    bentoml files changed

--- services/save_model.py
+++ services/save_model.py
@@ -3,5 +3,6 @@ import bentoml
 
 with open("/userRepoData/taeefnajib/Detecting-Fake-Notes-Using-Gradient-Boosting-Regressor/sidetrek/models/68cab49fa93f23e154b27ac7da89fec5.joblib", "rb") as f:
     model = joblib.load(f)
-    saved_model = bentoml.xgboost.save_model("example_model", model)
+    saved_model = bentoml.sklearn.save_model("example_model", model)
     print(saved_model) # This is required!
+
